3cd994ef4aa72572ecf403d64d1aa9e63f70cd21
sahmohseni <mohseni.seyedamirhessam@gmail.com>
2023-01-10 15:43:20 +0330

    changes applied

--- lib/data.g.dart
+++ lib/data.g.dart
@@ -1,6 +1,6 @@
 // GENERATED CODE - DO NOT MODIFY BY HAND
 
-part of 'data.dart';
+part of '../data.dart';
 
 // **************************************************************************
 // TypeAdapterGenerator
